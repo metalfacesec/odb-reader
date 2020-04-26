faae56b79eb551487d42fa126deb2cb48862f8b1
Metal Face <metalfacesec@gmail.com>
2020-04-26 16:28:40 -0400

    No data handling and more new PIDS

--- src/PIDFactory.js
+++ src/PIDFactory.js
@@ -3,22 +3,34 @@ const PID = require('./PID');
 class PIDFactory {
     static getPIDFromName(name) {
         switch (name.toLowerCase()) {
+            case 'supported_pids':
+                return new PID(name.toLowerCase(), '0100', '', '00', function (response) {
+                    return Math.floor(parseInt(response, 16));
+                })
             case 'rpm':
-                return new PID(name.toLowerCase(), '010C', 'rpm', '410C', function (response) {
-                    return (parseInt(response, 16) / 4);
+                return new PID(name.toLowerCase(), '010C', 'rpm', '0C', function (response) {
+                    return Math.floor(parseInt(response, 16) / 4);
                 });
             case 'fuel_pressure':
-                return new PID(name.toLowerCase(), '010A', 'kPa', '410A', function (response) {
-                    return (parseInt(response, 16) * 3);
+                return new PID(name.toLowerCase(), '010A', 'kPa', '0A', function (response) {
+                    return Math.floor(parseInt(response, 16) * 3);
                 });
             case 'maf_air_flow':
-                    return new PID(name.toLowerCase(), '0110', 'grams/sec', '4110', function (response) {
-                        return (parseInt(response, 16) / 100);
+                    return new PID(name.toLowerCase(), '0110', 'grams/sec', '10', function (response) {
+                        return Math.floor(parseInt(response, 16) / 100);
                     });
             case 'coolant_temp':
-                return new PID(name.toLowerCase(), '0105', 'C', '4105', function (response) {
-                    return (parseInt(response, 16) - 40);
+                return new PID(name.toLowerCase(), '0105', 'C', '05', function (response) {
+                    return Math.floor(parseInt(response, 16) - 40);
                 });
+            case 'engine_oil_temp':
+                return new PID(name.toLowerCase(), '015C', 'C', '5C', function (response) {
+                    return Math.floor(parseInt(response, 16) - 40);
+                })
+            case 'engine_coolant_temp':
+                    return new PID(name.toLowerCase(), '0167', 'C', '67', function (response) {
+                        return Math.floor(parseInt(response, 16));
+                    })
         }
         return null;
     }
--- src/odb-reader.js
+++ src/odb-reader.js
@@ -1,5 +1,8 @@
 const bluetooth = require('node-bluetooth');
 const pidFactory = require('./PIDFactory');
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
 
 class ODBReader {
 	constructor(enabledPids) {
@@ -7,6 +10,8 @@ class ODBReader {
 		this.device = new bluetooth.DeviceINQ();
 		this.deviceFound = false;
 		this.btDeviceConnection = null;
+		this.dataBuffer = new Buffer(256);
+		this.myEmitter = new MyEmitter();
 	}
 	getPidObjects(pidArray) {
 		let pidObjects = [];
@@ -45,25 +50,55 @@ class ODBReader {
 					if (err) return reject(err);
 
 					self.btDeviceConnection = connection;
+
+					connection.on('data', buffer => {
+						self.dataBuffer = Buffer.concat([self.dataBuffer, buffer]);
+						let clean_buffer_string = self.dataBuffer.toString().replace(/(\r\n|\n|\r)/gm, "").replace(/\s/g, '');
+
+						console.log(clean_buffer_string);
+						
+
+						if (clean_buffer_string.includes('NODATA')) {
+							let initialPIDOld = clean_buffer_string.split('NODATA')[0];
+							let initialPID = initialPIDOld.substring(initialPIDOld.length - 2);
+
+							self.enabledPIDs.forEach(pid => {
+								if (pid.responseIdentifier == initialPID.toString(16)) {
+									self.myEmitter.emit(pid.responseIdentifier, 'NO DATA');
+								}
+							});
+
+							self.dataBuffer = new Buffer(256);
+						}
+		
+						if (this.dataBuffer.toString().includes('>') && this.dataBuffer.toString().includes('41')) {
+							let clean_rez = this.dataBuffer.toString().replace(/\s+/g, '');
+							let initialPID = clean_rez.split('41')[1].substring(0, 2);
+							
+							self.enabledPIDs.forEach(pid => {
+								if (pid.responseIdentifier == initialPID.toString(16)) {
+									let value = clean_rez.split('41' + pid.responseIdentifier)[1].split('>')[0];
+									let cleanValue = pid.responseParser(value) + ' ' + pid.units;
+									
+									self.dataBuffer = new Buffer(256);
+									self.myEmitter.emit(pid.responseIdentifier, cleanValue);
+								}
+							});
+						}
+					});
+					
 					resolve(connection);
 				});
 			});
 		});
 	}
-	sendCommand(connection, command, responseIdentifier, responseParser) {
+	sendCommand(connection, command, responseIdentifier) {
 		let self = this;
 
-		// TODO: Need to remove the listener it isnt changing and is keeping the value constant of identifier
-		console.log("!!!!ID = " + responseIdentifier);
-		var ri = responseIdentifier;
-		console.log('!!!!clean res zplit1 = ' + ri + ' ');
-
-
 		return new Promise((resolve, reject) => {
-			let rez = '';
-			connection.on('data', data => {
-				console.log(data + '');
-				//console.log(self.enabledPIDs);
+			self.myEmitter.on(responseIdentifier, (data) => {
+				self.myEmitter = new MyEmitter();
+				resolve(data);
 			});
 			connection.write(new Buffer(command + '\r\n', 'utf-8'), function (err, count) {
 				if (err) {
@@ -76,11 +111,15 @@ class ODBReader {
 		while (true) {
 			for (let i = 0; i < this.enabledPIDs.length; i++) {
 				let currentPID = this.enabledPIDs[i];
-				console.log(currentPID.responseIdentifier);
-				console.log('!!!!running with current identifier above on pid = ' + currentPID.name);
-				let rez = await this.sendCommand(this.btDeviceConnection, currentPID.command, currentPID.responseIdentifier, currentPID.responseParser);
-				
-				console.log('** ' + currentPID.name + " = " + rez + '' + currentPID.units);
+
+				try {
+					let rez = await this.sendCommand(this.btDeviceConnection, currentPID.command, currentPID.responseIdentifier);
+					//let clean_rez = currentPID.responseParser(rez);
+					console.log(`${currentPID.name}: ${rez}`);
+				} catch(err) {
+					console.log(err);
+					return;
+				}
 			}
 		}
 	}
